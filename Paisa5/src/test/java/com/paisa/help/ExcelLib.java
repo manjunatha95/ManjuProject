594b7fa23d9cabb2a0e0c286c768a9c26bb9ed2f
Manjunatha <Manjunatha@192.168.1.5>
2017-07-27 17:49:33 +0530

    all commit

---
+++ Paisa5/src/test/java/com/paisa/help/ExcelLib.java
@@ -0,0 +1,29 @@
+package com.paisa.help;
+
+//import java.io.FileInputStream;
+
+
+public class ExcelLib 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+	
+	public static void setdata()
+	{
+		//String path = "E:/Workspace/Paisa5/Doc/TestCase.xlsx";
+		try
+		{
+			//FileInputStream fi = new FileInputStream(path);
+			//WorkbookFactory w1 = WorkbookFactory.create(fi);
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+
+}
